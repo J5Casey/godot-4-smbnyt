a0627e8d0cad338941d00e2725b0337c4fc406b1
J5Casey <149739665+J5Casey@users.noreply.github.com>
2024-01-07 17:15:37 -0800

    Added board with buttons and background

--- src/scenes/Board.gd
+++ src/scenes/Board.gd
@@ -1,19 +1,26 @@
-extends Control
+extends GridContainer
 
 var grid_size = Vector2(6, 6)
+var tile_size = Vector2(150, 150)
 
 func _ready():
 	create_grid()
 
 func create_grid():
+	columns = grid_size.y
+	
 	for row in range(grid_size.y):
 		for col in range(grid_size.x):
 			var tile = Panel.new()
-			tile.rect_size = Vector2(100, 100)  # Example: Set the size for each tile
-			tile.rect_position = Vector2(col * (tile.rect_size.x), row * (tile.rect_size.y))
+			tile.custom_minimum_size = tile_size
 			add_child(tile)
+
+		# Add any additional content or customization for each tile here
+			#var label = Label.new()
+			#label.text = str(row * grid_size.x + col + 1)
+			#tile.add_child(label)
 			
-			# Add any additional content or customization for each tile here
-			var label = Label.new()
-			label.text = str(row * grid_size.x + col + 1)  # Example: Assign a label or content
-			tile.add_child(label)
+			var button = Button.new()
+			button.text = str(row * grid_size.x + col + 1)
+			button.custom_minimum_size = tile_size
+			tile.add_child(button)
